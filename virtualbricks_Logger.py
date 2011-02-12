d663eb490a5e7f205a23b2dbceaae7c60aa54b51
Pierre-Louis Bonicoli pierre-louis.bonicoli@gmx.fr <Unknown>
2011-02-12 02:03:08 +0100

    allow a subclass to reimplement log methods

--- virtualbricks_Logger.py
+++ virtualbricks_Logger.py
@@ -7,11 +7,20 @@ class Logger(object):
 	def __init__(self, logger):
 		self.logger = logger
 
-		self.debug = self.logger.debug
-		self.info = self.logger.info
-		self.warning = self.logger.warning
-		self.error = self.logger.error
-		self.critical = self.logger.critical
+	def debug(self, *args, **kwargs):
+		self.logger.debug(*args, **kwargs)
+
+	def info(self, *args, **kwargs):
+		self.logger.info(*args, **kwargs)
+
+	def warning(self, *args, **kwargs):
+		self.logger.warning(*args, **kwargs)
+
+	def error(self, *args, **kwargs):
+		self.logger.error(*args, **kwargs)
+
+	def critical(self, *args, **kwargs):
+		self.logger.critical(*args, **kwargs)
 
 class ChildLogger(Logger):
 	def __init__(self, logger=None, name=None):
