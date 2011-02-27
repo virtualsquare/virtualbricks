6d866d3e57a58e6a4f60af5fe0aa1359ae0eae0d
Daniele Lacamera <root@danielinux.net>
2011-02-27 02:06:12 +0100

    * Added locale skeleton.po as starting point for new localizations * Locale nl_NL (Dutch): 10% complete.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 ##	along with this program; if not, write to the Free Software
 ##	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-SUPPORTED_LANGS=['it_IT']
+SUPPORTED_LANGS=['it_IT','nl_NL']
 
 from distutils.core import setup
 import sys,os
