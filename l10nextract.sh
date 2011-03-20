3e03a2bdd9e68f7d1650c6c0d4e7e9e8cdbfcbf2
Abbakus <abbakus@gmail.com>
2011-03-20 17:10:23 +0100

    * Fixed l10nextract.sh script due to new tree structure * Extracted and translated new messages to italian

--- l10nextract.sh
+++ l10nextract.sh
@@ -1,7 +1,7 @@
 COMMONS="--package-name=virtualbricks --package-version=0.3 --msgid-bugs-address=qemulator-list@createweb.de"
 JOIN="-j"
 LANGUAGES="it nl"
-SOURCES="share/*.glade virtualbricks/*"
+SOURCES="share/*.glade `find virtualbricks/ -type f`"
 xgettext -plocale/virtualbricks -ovirtualbricks.pot $JOIN $COMMONS $SOURCES
 for l in $LANGUAGES;
 do
