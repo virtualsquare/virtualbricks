73a49a3ba5bccd7a82023a9ebfd54e14ee01ff32
Francesco Apollonio <f.apollonio@ldlabs.org>
2011-03-22 10:48:38 +0100

    Added automatic revision number in about dialog

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+# Set version here
+VERSION_MAJOR=0
+VERSION_MINOR=4
+
+VERSION_MICRO=`cat .bzr/branch/last-revision | cut -d ' ' -f1`
+VERSION=$VERSION_MAJOR.$VERSION_MINOR.$VERSION_MICRO
+cat share/virtualbricks.template.glade | sed -e "s/___VERSION___/$VERSION/g" > share/virtualbricks.glade
+
 python setup.py install --record .filesinstalled
+rm -f share/virtualbricks.glade
