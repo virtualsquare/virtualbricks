2093f29a250b374ab92c786fe1f01498a0367e19
Marco Giusti <marco.giusti3@studio.unibo.it>
2013-04-22 13:54:38 +0200

    Fix #1171424

--- virtualbricks/tools.py
+++ virtualbricks/tools.py
@@ -16,7 +16,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import random
-from threading import Thread
+import threading
 import time
 import sys
 import re
@@ -30,24 +30,6 @@ def RandMac():
     mac = mac + "%02x:" % random.getrandbits(8)
     mac = mac + "%02x" % random.getrandbits(8)
     return mac
-
-
-class AutoSaveTimer(Thread):
-
-    def __init__(self, factory):
-        Thread.__init__(self)
-        self.autosave_timeout = 180
-        self.factory = factory
-
-    def run(self):
-        self.factory.debug("Autosaver started")
-        while (self.factory.running_condition):
-            for t in range(self.autosave_timeout):
-                time.sleep(1)
-                if not self.factory.running_condition:
-                    sys.exit()
-            self.factory.configfile.save(self.factory.settings.get(
-                'current_project'))
 
 
 def ValidName(name):
@@ -81,3 +63,59 @@ def NameNotInUse(factory, name):
         if i.name == name:
             return False
     return True
+
+
+class LoopingCall:
+
+    daemon = True
+    _name = None
+
+    def get_name(self):
+        if self._name is None:
+            return repr(self)
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    name = property(get_name, set_name)
+
+    def __init__(self, timeout, function, args=(), kwds={}):
+        self.__function = function
+        self.__args = args
+        self.__kwds = kwds
+        self.__timer = None
+        self.start(timeout)
+
+    def start(self, period=None):
+        self.stop()
+        if period is not None:
+            self.__period = period
+        self.__timer = threading.Timer(self.__period, self.__call)
+        self.__timer.daemon = self.daemon
+        self.__timer.name = self.get_name()
+        self.__timer.start()
+
+    def stop(self):
+        if self.__timer:
+            self.__timer.cancel()
+            self.__timer = None
+
+    def __call(self):
+        self.__function(*self.__args, **self.__kwds)
+        self.start()
+
+    def __repr__(self):
+        return '<LoopingCall func=%s, args=%s, kwds=%s>' % (self.__function,
+                                                            self.__args,
+                                                            self.__kwds)
+
+
+def AutosaveTimer(factory, timeout=180):
+    t = LoopingCall(timeout, factory.configfile.save,
+                    factory.settings.get( 'current_project'))
+    t.set_name("AutosaveTimer_%d" % timeout)
+    return t
+
+
+AutoSaveTimer = AutosaveTimer  # backward compatibility
