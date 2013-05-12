cfa130b13b84aaca9a23cb04d452eddb8b652795
Marco Giusti <marco.giusti3@studio.unibo.it>
2013-05-12 16:51:00 +0200

    Better cbset

--- virtualbricks/tests/test_bricks.py
+++ virtualbricks/tests/test_bricks.py
@@ -4,7 +4,10 @@ from virtualbricks.tests import unittest, stubs
 
 class TestBricks(unittest.TestCase):
 
-    pass
+    def test_get_cbset(self):
+        brick = bricks.Brick(stubs.FactoryStub(), "test")
+        cbset = brick.get_cbset("supercalifragilistichespiralidoso")
+        self.assertIs(cbset, None)
 
     # def test_basic_interface(self):
     #     factory = stubs.FactoryStub()
