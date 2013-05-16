03c7df23a66c568e40182734b5db24a86088edab
Marco Giusti <marco.giusti@posteo.de>
2013-05-16 14:41:55 +0200

    Test DeprecationWarning is raised for brick.on_config_changed()

--- virtualbricks/tests/test_bricks.py
+++ virtualbricks/tests/test_bricks.py
@@ -1,3 +1,5 @@
+import warnings
+
 from virtualbricks import bricks
 from virtualbricks.tests import unittest, stubs
 
@@ -8,6 +10,15 @@ class TestBricks(unittest.TestCase):
         brick = bricks.Brick(stubs.FactoryStub(), "test")
         cbset = brick.get_cbset("supercalifragilistichespiralidoso")
         self.assertIs(cbset, None)
+
+    def test_warnings(self):
+        brick = bricks.Brick(stubs.FactoryStub(), "test")
+        with warnings.catch_warnings(record=True)as w:
+            warnings.simplefilter("always")
+            brick.on_config_changed()
+            self.assertEqual(len(w), 1)
+            self.assertEquals(w[0].category, DeprecationWarning)
+
 
     # def test_basic_interface(self):
     #     factory = stubs.FactoryStub()
