1ef580d391db31d3aa918fd7ff355dbeffb5d0dc
Marco Giusti <marco.giusti@posteo.de>
2013-05-16 14:33:48 +0200

    Move brickconfig to base

--- virtualbricks/base.py
+++ virtualbricks/base.py
@@ -17,9 +17,90 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
+import logging
+
 import gobject
 
-from virtualbricks import brickconfig
+
+log = logging.getLogger(__name__)
+
+if False:  # pyflakes
+    _ = str
+
+
+class BrickConfig(dict):
+    """Generic configuration for Brick
+
+    >>> cfg = BrickConfig()
+    >>> cfg.enabled = True
+    >>> cfg['enabled'] == True
+    True
+    >>> cfg.enabled == True
+    True
+    >>> cfg.disabled = True
+    >>> cfg['disabled'] == True
+    True
+    >>> cfg.disabled == True
+    True
+    >>> from copy import deepcopy
+    >>> cfg2 = deepcopy(cfg)
+    """
+    def __getattr__(self, name):
+        """override dict.__getattr__"""
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        """override dict.__setattr__"""
+        self[name] = value
+        #Set value for running brick
+        self.set_running(name, value)
+
+    def set(self, attr):
+        kv = attr.split("=")
+        if len(kv) < 2:
+            return False
+        else:
+            val = ''
+            if len(kv) > 2:
+                val = '"'
+                for c in kv[1:]:
+                    val += c.lstrip('"').rstrip('"')
+                    val += "="
+                val = val.rstrip('=') + '"'
+            else:
+                val += kv[1]
+            self[kv[0]] = val
+            #Set value for running brick
+            self.set_running(kv[0], val)
+            return True
+
+    def set_obj(self, key, obj):
+        self[key] = obj
+
+    def set_running(self, key, value):
+        """
+        Set the value for the running brick,
+        if available and running
+        """
+        import inspect
+        stack = inspect.stack()
+        frame = stack[2][0]
+        obj = frame.f_locals.get('self', None)
+        if obj is not None:
+            if hasattr(obj, "get_cbset"):
+                setter = obj.get_cbset(key)
+                if setter is not None:
+                    log.debug(_("setter: setting value %s for key %s"), value,
+                              key)
+                    setter(value)
+
+    def dump(self):
+        keys = sorted(self.keys())
+        for k in keys:
+            print "%s=%s" % (k, self[k])
 
 
 class Base(gobject.GObject):
@@ -45,7 +126,7 @@ class Base(gobject.GObject):
         self.factory = factory
         self._name = name
         self.settings = self.factory.settings
-        self.cfg = brickconfig.BrickConfig()
+        self.cfg = BrickConfig()
 
     def get_type(self):
         return self.type
