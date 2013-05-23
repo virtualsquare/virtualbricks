a74f793a8e978c4a65627dc1143054002c47e2f6
Marco Giusti <marco.giusti@posteo.de>
2013-05-23 09:05:06 +0200

    Cleanup a bit the code

--- virtualbricks/gui/graphics.py
+++ virtualbricks/gui/graphics.py
@@ -30,179 +30,177 @@ import gtk.gdk
 
 
 __all__ = ["get_filename", "get_data", "get_image", "pixbuf_for_brick",
-		   "pixbuf_for_brick_at_size", "pixbuf_for_brick_type",
-		   "pixbuf_for_running_brick", "pixbuf_for_running_brick_at_size",
-		   "Node", "Topology"]
+           "pixbuf_for_brick_at_size", "pixbuf_for_brick_type",
+           "pixbuf_for_running_brick", "pixbuf_for_running_brick_at_size",
+           "Node", "Topology"]
 
 
 log = logging.getLogger('virtualbricks.gui')
 
 
 def get_filename(package, resource):
-	loader = pkgutil.get_loader(package)
-	mod = sys.modules.get(package) or loader.load_module(package)
-	if mod is None or not hasattr(mod, "__file__"):
-		return None
-	parts = resource.split("/")
-	parts.insert(0, os.path.dirname(mod.__file__))
-	return os.path.join(*parts)
+    loader = pkgutil.get_loader(package)
+    mod = sys.modules.get(package) or loader.load_module(package)
+    if mod is None or not hasattr(mod, "__file__"):
+        return None
+    parts = resource.split("/")
+    parts.insert(0, os.path.dirname(mod.__file__))
+    return os.path.join(*parts)
 
 
 def get_image(name):
-	return get_filename("virtualbricks.gui", "data/" + name)
+    return get_filename("virtualbricks.gui", "data/" + name)
 
 
 def has_custom_icon(brick):
-	return "icon" in brick.cfg and brick.cfg.icon != ""
+    return "icon" in brick.cfg and brick.cfg.icon != ""
 
 
 def brick_icon(brick):
-	if not has_custom_icon(brick):
-		return get_filename("virtualbricks.gui",
-						"data/" + brick.get_type().lower() + ".png")
-	else:
-		return brick.cfg.icon
+    if not has_custom_icon(brick):
+        return get_filename("virtualbricks.gui",
+                        "data/" + brick.get_type().lower() + ".png")
+    else:
+        return brick.cfg.icon
 
 
 def is_running(brick):
-	if hasattr(brick, "proc"):
-		return brick.proc is not None
-	elif hasattr(brick, "active"):
-		return brick.active
-	return True
+    if hasattr(brick, "proc"):
+        return brick.proc is not None
+    elif hasattr(brick, "active"):
+        return brick.active
+    return True
 
 
 def saturate_if_stopped(brick, pixbuf):
-	if is_running(brick):
-		return pixbuf
-	else:
-		pixbuf.saturate_and_pixelate(pixbuf, 0.0, True)
-		return pixbuf
+    if is_running(brick):
+        return pixbuf
+    else:
+        pixbuf.saturate_and_pixelate(pixbuf, 0.0, True)
+        return pixbuf
 
 
 def pixbuf_for_brick_at_size(brick, width, height):
-	filename = brick_icon(brick)
-	pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(filename, width, height)
-	return saturate_if_stopped(brick, pixbuf)
+    filename = brick_icon(brick)
+    pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(filename, width, height)
+    return saturate_if_stopped(brick, pixbuf)
 
 
 def pixbuf_for_brick(brick):
-	filename = brick_icon(brick)
-	pixbuf = gtk.gdk.pixbuf_new_from_file(filename)
-	return saturate_if_stopped(brick, pixbuf)
+    filename = brick_icon(brick)
+    pixbuf = gtk.gdk.pixbuf_new_from_file(filename)
+    return saturate_if_stopped(brick, pixbuf)
 
 
 def pixbuf_for_brick_type(type):
-	filename = get_filename("virtualbricks.gui", "data/%s.png" % type.lower())
-	return gtk.gdk.pixbuf_new_from_file(filename)
+    filename = get_filename("virtualbricks.gui", "data/%s.png" % type.lower())
+    return gtk.gdk.pixbuf_new_from_file(filename)
 
 
 def pixbuf_for_running_brick(brick):
-	return gtk.gdk.pixbuf_new_from_file(brick_icon(brick))
+    return gtk.gdk.pixbuf_new_from_file(brick_icon(brick))
 
 
 def pixbuf_for_running_brick_at_size(brick, witdh, height):
-	return gtk.gdk.pixbuf_new_from_file_at_size(brick_icon(brick),
-			witdh, height)
+    return gtk.gdk.pixbuf_new_from_file_at_size(brick_icon(brick),
+            witdh, height)
 
 
 class Node:
-	def __init__(self, topology, name, x, y, thresh = 50):
-		self.x = x
-		self.y = y
-		self.thresh = thresh
-		self.name = name
-		self.parent = topology
-	def here(self, x, y):
-		if abs(x + self.parent.x_adj - self.x) < self.thresh and abs(y + self.parent.y_adj - self.y) < self.thresh:
-			return True
-		else:
-			return False
 
-class Topology():
+    def __init__(self, topology, name, x, y, thresh=50):
+        self.x = x
+        self.y = y
+        self.thresh = thresh
+        self.name = name
+        self.parent = topology
 
-	def __init__(self, widget, bricks_model, scale=1.00, orientation="LR", export=None, tempdir="/tmp/"):
-		self.topowidget = widget
-		self.topo = pgv.AGraph()
-
-		self.topo.graph_attr['rankdir']=orientation
+    def here(self, x, y):
+        return (abs(x + self.parent.x_adj - self.x) < self.thresh and
+                abs(y + self.parent.y_adj - self.y) < self.thresh)
 
 
-		self.topo.graph_attr['ranksep']='2.0'
-		self.nodes = []
-		self.x_adj = 0.0
-		self.y_adj = 0.0
+class Topology:
 
-		# Add nodes
-		sg = self.topo.add_subgraph([],name="switches_rank")
-		sg.graph_attr['rank'] = 'same'
-		for row in bricks_model:
-			b = row[0]
-			self.topo.add_node(b.name)
-			n = self.topo.get_node(b.name)
-			n.attr['shape']='none'
-			n.attr['fontsize']='9'
-			n.attr['image'] = brick_icon(b)
+    def __init__(self, widget, bricks_model, scale=1.00, orientation="LR",
+                 export=None, tempdir="/tmp/"):
+        self.topowidget = widget
+        self.topo = pgv.AGraph()
+        self.topo.graph_attr['rankdir'] = orientation
+        self.topo.graph_attr['ranksep'] = '2.0'
+        self.nodes = []
+        self.x_adj = 0.0
+        self.y_adj = 0.0
 
-		for row in bricks_model:
-			b = row[0]
-			loop = 0
-			for e in b.plugs:
-				if e.sock is not None:
-					if (b.get_type() == 'Tap'):
-						self.topo.add_edge(b.name, e.sock.brick.name)
-						e = self.topo.get_edge(b.name, e.sock.brick.name)
-					elif len(b.plugs) == 2:
-						if loop == 0:
-							self.topo.add_edge(e.sock.brick.name, b.name)
-							e = self.topo.get_edge(e.sock.brick.name, b.name)
-						else:
-							self.topo.add_edge(b.name, e.sock.brick.name)
-							e = self.topo.get_edge(b.name, e.sock.brick.name)
-					elif loop < (len(b.plugs) + 1) / 2:
-						self.topo.add_edge(e.sock.brick.name, b.name)
-						e = self.topo.get_edge(e.sock.brick.name, b.name)
-					else:
-						self.topo.add_edge(b.name, e.sock.brick.name)
-						e = self.topo.get_edge(b.name, e.sock.brick.name)
-					loop+=1
-					e.attr['dir'] = 'none'
-					e.attr['color'] = 'black'
-					e.attr['name'] = "      "
-					e.attr['decorate']='true'
+        # Add nodes
+        sg = self.topo.add_subgraph([], name="switches_rank")
+        sg.graph_attr['rank'] = 'same'
+        for row in bricks_model:
+            b = row[0]
+            self.topo.add_node(b.name)
+            n = self.topo.get_node(b.name)
+            n.attr['shape'] = 'none'
+            n.attr['fontsize'] = '9'
+            n.attr['image'] = brick_icon(b)
 
-		#draw and save
-		self.topo.write(tempdir+"vde.dot")
-		self.topo.layout('dot')
-		self.topo.draw(tempdir+"vde_topology.png")
-		self.topo.draw(tempdir+"vde_topology.plain")
+        for row in bricks_model:
+            b = row[0]
+            loop = 0
+            for e in b.plugs:
+                if e.sock is not None:
+                    if b.get_type() == 'Tap':
+                        self.topo.add_edge(b.name, e.sock.brick.name)
+                        e = self.topo.get_edge(b.name, e.sock.brick.name)
+                    elif len(b.plugs) == 2:
+                        if loop == 0:
+                            self.topo.add_edge(e.sock.brick.name, b.name)
+                            e = self.topo.get_edge(e.sock.brick.name, b.name)
+                        else:
+                            self.topo.add_edge(b.name, e.sock.brick.name)
+                            e = self.topo.get_edge(b.name, e.sock.brick.name)
+                    elif loop < (len(b.plugs) + 1) / 2:
+                        self.topo.add_edge(e.sock.brick.name, b.name)
+                        e = self.topo.get_edge(e.sock.brick.name, b.name)
+                    else:
+                        self.topo.add_edge(b.name, e.sock.brick.name)
+                        e = self.topo.get_edge(b.name, e.sock.brick.name)
+                    loop += 1
+                    e.attr['dir'] = 'none'
+                    e.attr['color'] = 'black'
+                    e.attr['name'] = "      "
+                    e.attr['decorate'] = 'true'
 
-		img = Image.open(tempdir+"vde_topology.png")
-		x_siz, y_siz = img.size
-		for line in open(tempdir+"vde_topology.plain").readlines():
-			#arg  = line.rstrip('\n').split(' ')
-			arg = re.split('\s+', line.rstrip('\n'))
-			if arg[0] == 'graph':
-				if(float(arg[2]) != 0 and float(arg[3]) !=0 ):
-					x_fact = scale * (x_siz / float(arg[2]))
-					y_fact = scale * (y_siz / float(arg[3]))
-				else:
-					x_fact = 1
-					y_fact = 1
-			elif arg[0] == 'node':
-				if(float(arg[2]) !=0 and float(arg[3] != 0)):
-					x = scale * (x_fact * float(arg[2]))
-					y = scale * (y_siz - y_fact * float(arg[3]))
-				else:
-					x = scale * (x_fact)
-					y = scale * (y_siz - y_fact)
-				self.nodes.append(Node(self, arg[1],x,y))
-		# Display on the widget
-		if scale < 1.00:
-			img.resize((x_siz * scale, y_siz * scale)).save(tempdir+"vde_topology.png")
+        #draw and save
+        self.topo.write(tempdir + "vde.dot")
+        self.topo.layout('dot')
+        self.topo.draw(tempdir + "vde_topology.png")
+        self.topo.draw(tempdir + "vde_topology.plain")
 
-		self.topowidget.set_from_file(tempdir+"vde_topology.png")
-		if export:
-			img.save(export)
+        img = Image.open(tempdir + "vde_topology.png")
+        x_siz, y_siz = img.size
+        for line in open(tempdir + "vde_topology.plain").readlines():
+            #arg  = line.rstrip('\n').split(' ')
+            arg = re.split('\s+', line.rstrip('\n'))
+            if arg[0] == 'graph':
+                if float(arg[2]) != 0 and float(arg[3]) != 0:
+                    x_fact = scale * (x_siz / float(arg[2]))
+                    y_fact = scale * (y_siz / float(arg[3]))
+                else:
+                    x_fact = 1
+                    y_fact = 1
+            elif arg[0] == 'node':
+                if float(arg[2]) != 0 and float(arg[3] != 0):
+                    x = scale * (x_fact * float(arg[2]))
+                    y = scale * (y_siz - y_fact * float(arg[3]))
+                else:
+                    x = scale * (x_fact)
+                    y = scale * (y_siz - y_fact)
+                self.nodes.append(Node(self, arg[1], x, y))
+        # Display on the widget
+        if scale < 1.00:
+            img.resize((x_siz * scale, y_siz * scale)).save(
+                tempdir + "vde_topology.png")
 
-# vim: se noet :
+        self.topowidget.set_from_file(tempdir + "vde_topology.png")
+        if export:
+            img.save(export)
