4722ddccf8ca8c34db1dcf9f5e627c1e3c335683
Marco Giusti <marco.giusti@posteo.de>
2013-06-12 15:16:34 +0200

    Do again config tests

--- virtualbricks/tests/test_base.py
+++ virtualbricks/tests/test_base.py
@@ -2,11 +2,12 @@ import os
 import StringIO
 
 from virtualbricks import base
-from virtualbricks.tests import unittest, Skip
+from virtualbricks.tests import unittest, Skip, stubs
 
 
 marker = object()
-DUMP = """bool=
+DUMP = """[Stub:base]
+bool=
 float=0.1
 int=43
 spinint=31
@@ -39,6 +40,13 @@ class Config2(Config1):
         "spinint": base.SpinInt(32, 1, 128),
     }
 
+
+class BaseStub(base.Base):
+
+    type = "Stub"
+    config_factory = Config2
+
+
 class TestNewConfig(unittest.TestCase):
 
     def setUp(self):
@@ -68,44 +76,6 @@ class TestNewConfig(unittest.TestCase):
     def test_getattr(self):
         self._test_string(getattr)
 
-    @Skip("method moved to base.Base")
-    def test_save_to(self):
-        sio = StringIO.StringIO()
-        self.cfg2.save_to(sio)
-        self.assertEqual(sio.getvalue(), "\n")
-        self.cfg2["bool"] = False
-        self.cfg2["float"] = 0.1
-        self.cfg2["int"] = 43
-        self.cfg2["spinint"] = 31
-        self.cfg2["str"] = "b"
-        sio.seek(0)
-        self.cfg2.save_to(sio)
-        self.assertEqual(sio.getvalue(), DUMP)
-
-    @Skip("method moved to base.Base")
-    def test_restore_from(self):
-        sio = StringIO.StringIO(DUMP)
-        sio.seek(-1, os.SEEK_END)
-        sio.write("# this is a comment")
-        sio.seek(0)
-        self.cfg2.load_from(sio)
-        for p, v in (("bool", False), ("float", 0.1), ("int", 43),
-                     ("spinint", 31), ("str", "b")):
-            self.assertEqual(self.cfg2[p], v)
-        cur = sio.tell()
-        sio.seek(0, os.SEEK_END)
-        self.assertEqual(cur, sio.tell())
-
-    @Skip("method moved to base.Base")
-    def test_restore_advanced(self):
-        sio = StringIO.StringIO(DUMP2)
-        sio.seek(0)
-        self.cfg2.load_from(sio)
-        for p, v in (("bool", True), ("float", 0.0), ("int", 43),
-                     ("spinint", 32), ("str", "a")):
-            self.assertEqual(self.cfg2[p], v)
-        self.assertEqual(sio.tell(), 7)
-
     def test_dict_interface(self):
         self.assertIn("str", self.cfg2)
         self.cfg2["str"] = "b"
@@ -119,3 +89,47 @@ class TestNewConfig(unittest.TestCase):
         self.assertEquals(list(sorted(self.cfg2.iteritems())),
                           [("bool", "*"), ("float", "0.0"), ("int", "42"),
                            ("obj", marker), ("spinint", "32"), ("str", "a")])
+
+
+class TestBase(unittest.TestCase):
+
+    def setUp(self):
+        self.factory = stubs.FactoryStub()
+        self.brick = BaseStub(self.factory, "base")
+
+    def test_save_to(self):
+        sio = StringIO.StringIO()
+        self.brick.save_to(sio)
+        self.assertEqual(sio.getvalue(), "[Stub:base]\n\n")
+        self.brick.cfg["bool"] = False
+        self.brick.cfg["float"] = 0.1
+        self.brick.cfg["int"] = 43
+        self.brick.cfg["spinint"] = 31
+        self.brick.cfg["str"] = "b"
+        sio.seek(0)
+        self.brick.save_to(sio)
+        self.assertEqual(sio.getvalue(), DUMP)
+
+    def test_restore_from(self):
+        sio = StringIO.StringIO(DUMP)
+        sio.seek(-1, os.SEEK_END)
+        sio.write("# this is a comment")
+        sio.seek(0)
+        # skip the first line
+        sio.readline()
+        self.brick.load_from(sio)
+        for p, v in (("bool", False), ("float", 0.1), ("int", 43),
+                     ("spinint", 31), ("str", "b")):
+            self.assertEqual(self.brick.cfg[p], v)
+        cur = sio.tell()
+        sio.seek(0, os.SEEK_END)
+        self.assertEqual(cur, sio.tell())
+
+    def test_restore_advanced(self):
+        sio = StringIO.StringIO(DUMP2)
+        sio.seek(0)
+        self.brick.load_from(sio)
+        for p, v in (("bool", True), ("float", 0.0), ("int", 43),
+                     ("spinint", 32), ("str", "a")):
+            self.assertEqual(self.brick.cfg[p], v)
+        self.assertEqual(sio.tell(), 7)
