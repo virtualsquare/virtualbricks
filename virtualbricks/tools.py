7c2db6cc6ce7dd2a3f52e85240da95758eac0091
Francesco Apollonio <f.apollonio@ldlabs.org>
2013-09-16 16:07:49 +0200

    Bug #1225995: fixed

--- virtualbricks/tools.py
+++ virtualbricks/tools.py
@@ -41,7 +41,6 @@ def random_mac():
 RandMac = random_mac
 MAC_RE = re.compile(r"^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$")
 
-
 def mac_is_valid(mac):
     return bool(MAC_RE.match(mac))
 
@@ -72,24 +71,45 @@ def stack_trace():
 def check_missing(path, files):
     return [f for f in files if not os.access(os.path.join(path, f), os.X_OK)]
 
+vde_bins = ["vde_switch", "vde_plug", "vde_cryptcab", "dpipe", "vdeterm",
+    "vde_plug2tap", "wirefilter", "vde_router"]
+
+qemu_bins = ["qemu", "kvm", "qemu-system-arm", "qemu-system-cris",
+    "qemu-system-i386", "qemu-system-m68k", "qemu-system-microblaze",
+    "qemu-system-mips", "qemu-system-mips64", "qemu-system-mips64el",
+    "qemu-system-mipsel", "qemu-system-ppc", "qemu-system-ppc64",
+    "qemu-system-ppcemb", "qemu-system-sh4", "qemu-system-sh4eb",
+    "qemu-system-sparc", "qemu-system-sparc64", "qemu-system-x86_64",
+    "qemu-img"]
+
+def bin2brick(bin_name):
+
+    brick_bin = {'Switch': 'vde_switch',
+            'Wire': 'vde_plug',
+            'Wirefilter': 'wirefilter',
+            'Tap': 'vde_plug2tap',
+            'TunnelConnect': 'vde_cryptcab',
+            'TunnelListen': 'vde_cryptcab',
+            'Capture': 'vde_pcapplug',
+            'Router': 'vde_router'
+    }
+
+    brick_list = []
+    if bin_name in qemu_bins:
+        brick_list = ['Qemu']
+    else:
+        for i in brick_bin.iterkeys():
+            if brick_bin[i] == bin_name:
+                brick_list.append(i)
+    
+    return brick_list
 
 def check_missing_vde(path):
-    bins = ["vde_switch", "vde_plug", "vde_cryptcab", "dpipe", "vdeterm",
-            "vde_plug2tap", "wirefilter", "vde_router"]
-    return check_missing(path, bins)
-
+    return check_missing(path, vde_bins)
 
 def check_missing_qemu(path):
-    bins = ["qemu", "kvm", "qemu-system-arm", "qemu-system-cris",
-            "qemu-system-i386", "qemu-system-m68k", "qemu-system-microblaze",
-            "qemu-system-mips", "qemu-system-mips64", "qemu-system-mips64el",
-            "qemu-system-mipsel", "qemu-system-ppc", "qemu-system-ppc64",
-            "qemu-system-ppcemb", "qemu-system-sh4", "qemu-system-sh4eb",
-            "qemu-system-sparc", "qemu-system-sparc64", "qemu-system-x86_64",
-            "qemu-img"]
-    missing = check_missing(path, bins)
-    return missing, sorted(set(bins) - set(missing))
-
+    missing = check_missing(path, qemu_bins)
+    return missing, sorted(set(qemu_bins) - set(missing))
 
 def check_kvm(path):
     if not os.access(os.path.join(path, "kvm"), os.X_OK):
