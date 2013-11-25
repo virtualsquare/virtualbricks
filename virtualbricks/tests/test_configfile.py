19284c0e02db62f0411156abc86935a5781dbd3c
Marco Giusti <marco.giusti@posteo.de>
2013-11-25 12:59:51 +0100

    Add IBuilder interface

--- virtualbricks/tests/test_configfile.py
+++ virtualbricks/tests/test_configfile.py
@@ -4,7 +4,7 @@ import StringIO
 from twisted.python import log, filepath
 
 from virtualbricks import configfile, configparser
-from virtualbricks.tests import unittest, stubs
+from virtualbricks.tests import unittest, stubs, LoggingObserver
 
 
 CONFIG1 = """
@@ -25,10 +25,11 @@ tdf=*
 link|sender|sw1_port|rtl8139|00:aa:79:71:be:61
 """
 
+
 class TestConfigFile(unittest.TestCase):
 
     def add_observer(self):
-        observer = stubs.LoggingObserver()
+        observer = LoggingObserver()
         log.addObserver(observer.emit)
         self.addCleanup(log.removeObserver, observer.emit)
         return observer
@@ -38,6 +39,31 @@ class TestConfigFile(unittest.TestCase):
         config.touch()
         backup = config.sibling(config.basename() + "~")
         return config, backup
+
+    def test_exported_interface(self):
+        """
+        Test the exported interface. If the interface change, this test
+        should change accordingly.
+        """
+
+        self.assertEqual(configfile.__all__,
+            ["BrickBuilder", "ConfigFile", "EventBuilder", "ImageBuilder",
+             "LinkBuilder", "SockBuilder", "log_events", "restore",
+             "safe_save", "save"])
+
+    def test_exported_log_events(self):
+        """
+        Test the log events exported by configfile. If the list of events
+        changes, this test should change accordingly.
+        """
+        self.assertEqual(configfile.log_events,
+            [configfile.link_type_error, configfile.brick_not_found,
+             configfile.sock_not_found, configfile.link_added,
+             configfile.cannot_save_backup, configfile.project_saved,
+             configfile.cannot_restore_backup, configfile.backup_restored,
+             configfile.image_found, configfile.skip_image,
+             configfile.skip_image_noa, configfile.config_dump,
+             configfile.open_project, configfile.config_save_error])
 
     def test_restore_backup_does_not_exists(self):
         """Try to restore a backup that does not exists."""
@@ -53,7 +79,7 @@ class TestConfigFile(unittest.TestCase):
     def test_restore_backup(self):
         """Restore a backup."""
 
-        observer = self.add_observer()
+        # observer = self.add_observer()
         config, backup = self.create_config_backup()
         backup.touch()
         configfile.restore_backup(config, backup)
@@ -89,6 +115,80 @@ class TestConfigFile(unittest.TestCase):
     def test_restore(self):
         config = configfile.ConfigFile()
         self.assertRaises(IOError, config.restore, None, self.mktemp())
+
+    def _add_observer(self, event=None):
+        observer = LoggingObserver()
+        if event:
+            self.addCleanup(event.tap(observer, configfile.logger.publisher))
+        return observer
+
+    def test_sock_builder_does_not_find_brick(self):
+        """
+        The sock builder does not find the sock owner, no exception is raised
+        and a warning is emitted.
+        """
+
+        BRICKNAME = "new_brick"
+        observer = self._add_observer(configfile.brick_not_found)
+        link = configparser.Link("sock", BRICKNAME, "name", "model", "mac")
+        configfile.SockBuilder().load_from(stubs.FactoryStub(), link)
+        self.assertEqual(len(observer), 1)
+        self.assertEqual(observer[0]["brick"], BRICKNAME)
+
+    def test_sock_builder(self):
+        """Create a sock from a Link object."""
+
+        BRICKNAME = "new_brick"
+        factory = stubs.FactoryStub()
+        brick = factory.new_brick("vm", BRICKNAME)
+        link = configparser.Link("sock", BRICKNAME, None, None, None)
+        configfile.SockBuilder().load_from(factory, link)
+        self.assertEqual(len(factory.socks), 1)
+        self.assertIs(factory.socks[0].brick, brick)
+
+    def test_link_builder_brick_not_found(self):
+        """
+        The sock builder does not find the sock owner, no exception is raised
+        and a warning is emitted.
+        """
+
+        BRICKNAME = "new_brick"
+        observer = self._add_observer(configfile.brick_not_found)
+        link = configparser.Link("link", BRICKNAME, "name", "model", "mac")
+        configfile.LinkBuilder().load_from(stubs.FactoryStub(), link)
+        self.assertEqual(len(observer), 1)
+        self.assertEqual(observer[0]["brick"], BRICKNAME)
+
+    def test_link_builder_sock_not_found(self):
+        """
+        The link builder does not find the sock, no exception is raised and a
+        warning is emitted.
+        """
+
+        BRICKNAME = "new_brick"
+        SOCKNAME = "nonexistent_sock"
+        factory = stubs.FactoryStub()
+        observer = self._add_observer(configfile.sock_not_found)
+        brick = factory.new_brick("vm", BRICKNAME)
+        self.assertEqual(len(brick.plugs), 0)
+        link = configparser.Link("link", BRICKNAME, SOCKNAME, "model", "mac")
+        configfile.LinkBuilder().load_from(factory, link)
+        self.assertEqual(len(brick.plugs), 0)
+        self.assertEqual(len(observer), 1)
+        self.assertEqual(observer[0]["sockname"], SOCKNAME)
+
+    def test_link_builder(self):
+        """Create a link from a Link object."""
+
+        BRICKNAME = "new_brick"
+        factory = stubs.FactoryStub()
+        brick = factory.new_brick("vm", BRICKNAME)
+        brick.add_sock()
+        self.assertEqual(len(brick.plugs), 0)
+        NICKNAME = "{0}_sock_eth0".format(BRICKNAME)
+        link = configparser.Link("link", BRICKNAME, NICKNAME, "model", "mac")
+        configfile.LinkBuilder().load_from(factory, link)
+        self.assertEqual(len(brick.plugs), 1)
 
 
 class TestParser(unittest.TestCase):
