37acdb972e1699a65ed754c41b553874f231409f
Marco Giusti <marco.giusti@posteo.de>
2014-01-31 12:36:18 +0100

    Fix regression in brick configuration

--- virtualbricks/tests/test_gui.py
+++ virtualbricks/tests/test_gui.py
@@ -2,7 +2,7 @@ from twisted.trial import unittest
 
 import gtk
 
-from virtualbricks.gui import gui, _gui
+from virtualbricks.gui import gui, _gui, interfaces
 from virtualbricks.tests import stubs, create_manager
 from virtualbricks.tests.test_project import TestBase
 
@@ -182,3 +182,195 @@ class TestReadme(TestBase, unittest.TestCase):
         self.assertEqual(project.get_description(), "")
         readme_tab.on_quit()
         self.assertEqual(project.get_description(), DESC)
+
+
+class DumbGui:
+
+    def __init__(self, factory):
+        self.brickfactory = factory
+
+
+class TestController(unittest.TestCase):
+
+    def setUp(self):
+        self.factory = stubs.Factory()
+        self.gui = DumbGui(self.factory)
+        self.brick = self.get_brick()
+        self.controller = interfaces.IConfigController(self.brick)
+
+    def get_brick(self):
+        raise NotImplementedError()
+
+    def get_object(self, name):
+        return self.controller.get_object(name)
+
+    def configure_brick(self):
+        self.controller.configure_brick(self.gui)
+
+    def get_config_view(self):
+        self.controller.get_config_view(self.gui)
+
+    def _assert_active_equal(self, name, status):
+        self.assertEqual(self.controller.get_object(name).get_active(), status)
+
+    def assert_active(self, name):
+        self._assert_active_equal(name, True)
+
+    def assert_not_active(self, name):
+        self._assert_active_equal(name, False)
+
+    def assert_spinbutton_value(self, name, value):
+        self.assertEqual(self.controller.get_object(name).get_value_as_int(),
+                         value)
+
+    def assert_parameter_equal(self, name, value):
+        self.assertEqual(self.brick.get(name), value)
+
+    def assert_parameters_equal(self, names=(), values=(), **kwds):
+        for name, value in zip(names, values):
+            self.assert_parameter_equal(name, value)
+        for name, value in kwds.iteritems():
+            self.assert_parameter_equal(name, value)
+
+    def assert_text_equal(self, name, text):
+        self.assertEqual(self.controller.get_object(name).get_text(), text)
+
+
+class TestSwitchController(TestController):
+
+    def get_brick(self):
+        from virtualbricks.switches import Switch
+        return Switch(self.factory, "test")
+
+    def test_view(self):
+        """The initial status of the switch controller."""
+
+        self.brick.set({"fstp": False, "hub": False, "numports": 2})
+        self.get_config_view()
+        self.assert_not_active("fstp_checkbutton")
+        self.assert_not_active("hub_checkbutton")
+        self.assert_spinbutton_value("ports_spinbutton", 2)
+
+    def test_config(self):
+        """Set the switch parameters."""
+
+        self.assert_parameters_equal(("fstp", "hub", "numports"),
+                                     (False, False, 32))
+        self.get_object("fstp_checkbutton").set_active(True)
+        self.get_object("hub_checkbutton").set_active(False)
+        self.get_object("ports_spinbutton").set_value(31)
+        self.configure_brick()
+        self.assert_parameters_equal(("fstp", "hub", "numports"),
+                                     (True, False, 31))
+
+
+class TestSwitchWrapperController(TestController):
+
+    PATH = "/foo/bar"
+
+    def get_brick(self):
+        from virtualbricks.switches import SwitchWrapper
+        return SwitchWrapper(self.factory, "test")
+
+    def test_view(self):
+        """The initial status of the switch wrapper."""
+
+        self.brick.set({"path": self.PATH})
+        self.get_config_view()
+        self.assert_text_equal("entry", self.PATH)
+
+    def test_config(self):
+        """Set the switch wrapper parameters."""
+
+        self.assert_parameter_equal("path", "")
+        self.get_object("entry").set_text(self.PATH)
+        self.configure_brick()
+        self.assert_parameter_equal("path", self.PATH)
+
+
+class TestTapController(TestController):
+
+    def get_brick(self):
+        from virtualbricks.tuntaps import Tap
+        return Tap(self.factory, "test")
+
+    def test_view(self):
+        """The initial status of the tap."""
+
+        cfg = {"ip": "0.0.0.0",
+               "nm": "0.0.0.0",
+               "gw": "0.0.0.0",
+               "mode": "dhcp"}
+        self.brick.set(cfg)
+        self.get_config_view()
+        self.assert_text_equal("ip_entry", "0.0.0.0")
+        self.assert_text_equal("nm_entry", "0.0.0.0")
+        self.assert_text_equal("gw_entry", "0.0.0.0")
+        self.assert_not_active("nocfg_radiobutton")
+        self.assert_active("dhcp_radiobutton")
+        self.assert_active("manual_radiobutton")
+    test_view.todo = "Implement test utility for the plugmixin"
+
+    def assert_initial(self):
+        """Assert initial status."""
+
+        self.assert_parameters_equal(ip="10.0.0.1",
+                                     nm="255.255.255.0",
+                                     gw="",
+                                     mode="off")
+
+    def test_config_nocfg(self):
+        """Set the tap parameters for no network configuration."""
+
+        self.assert_initial()
+        self.get_object("nocfg_radiobutton").set_active(True)
+        self.configure_brick()
+        self.assert_parameters_equal(ip="10.0.0.1",
+                                     nm="255.255.255.0",
+                                     gw="",
+                                     mode="off")
+
+    def test_config_dhcp(self):
+        """Set the tap parameters for dhcp."""
+
+        self.assert_initial()
+        self.get_object("dhcp_radiobutton").set_active(True)
+        self.configure_brick()
+        self.assert_parameters_equal(ip="10.0.0.1",
+                                     nm="255.255.255.0",
+                                     gw="",
+                                     mode="dhcp")
+
+    def test_config_manual(self):
+        """Set the tap parameters for dhcp."""
+
+        IP = "192.168.1.1"
+        NM = "255.255.0.0"
+        GW = "192.168.179.1"
+        self.assert_initial()
+        self.get_object("manual_radiobutton").set_active(True)
+        self.get_object("ip_entry").set_text(IP)
+        self.get_object("nm_entry").set_text(NM)
+        self.get_object("gw_entry").set_text(GW)
+        self.configure_brick()
+        self.assert_parameters_equal(ip=IP, nm=NM, gw=GW, mode="manual")
+
+
+class TestCaptureController(TestController):
+
+    def get_brick(self):
+        from virtualbricks.tuntaps import Capture
+        return Capture(self.factory, "test")
+
+    def test_view(self):
+        """The initial status of the capture interface."""
+
+        self.get_config_view()
+    test_view.todo = "Implement test utility for the plugmixin"
+
+    def test_config(self):
+        """Set the capture interface parameters."""
+
+        self.assert_parameter_equal("iface", "")
+        self.fail("TODO")
+    test_config.todo = "Implement test utility for the plugmixin"
