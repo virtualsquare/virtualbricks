987bbc2ceecb4d94c6660d8bbc78972881097103
Marco Giusti <marco.giusti3@studio.unibo.it>
2020-01-01 20:51:44 +0100

    Fix iter_disk_images

--- virtualbricks/brickfactory.py
+++ virtualbricks/brickfactory.py
@@ -235,7 +235,7 @@ class BrickFactory(object):
         return self._disk_images_path_idx.get(path)
 
     def iter_disk_images(self):
-        return iter(self._disk_images_path_idx)
+        return iter(self._disk_images)
 
     # Bricks
 
