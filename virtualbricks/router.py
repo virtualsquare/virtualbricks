bf224f6220e8a8d7f0be1d3a7cd87b83dfee95c6
Marco Giusti <marco.giusti@posteo.de>
2020-01-06 22:18:01 +0100

    Rename _spawn.py to spawn.py

--- virtualbricks/router.py
+++ virtualbricks/router.py
@@ -16,7 +16,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from virtualbricks import bricks
-from virtualbricks._spawn import abspath_vde
+from virtualbricks.spawn import abspath_vde
 
 
 class Router(bricks.Brick):
