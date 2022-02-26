587d135fdb491e10bc550d0804c1d5d7a7c94112
Marco Giusti <marco.giusti@posteo.de>
2022-02-26 18:57:14 +0100

    Configuration for www.virtualbricks.it

--- publishconf.py
+++ publishconf.py
@@ -8,18 +8,10 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 # SITEURL = 'https://virtualsquare.github.io/virtualbricks'
-SITEURL = 'http://wiki.virtualsquare.org/virtualbricks'
+SITEURL = 'http://www.virtualbricks.it'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
-
-# Social widget
-SOCIAL = (
-    ('Carlo Caini', '/virtualbricks/pages/authors.html#carlo-caini'),
-    ('Daniele Lacamera', '/virtualbricks/pages/authors.html#daniele-lacamera'),
-    ('Pietrofrancesco Apollonio', '/virtualbricks/pages/authors.html#pietrofrancesco-apollonio'),
-    ('Marco Giusti', '/virtualbricks/pages/authors.html#marco-giusti'),
-)
